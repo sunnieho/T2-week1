b3864f959fa90ff23c0a3ab882efbfe5310ba3cb
Sunnie Ho <sunnieho@Sunnies-MacBook-Air.local>
2025-07-01 13:33:10 +1000

    rename public to docs

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('docs'));
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
